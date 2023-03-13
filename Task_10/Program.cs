6bcb2eada2d53823309223131ddd5157e988a289
Aleksey <Sad_libra1988@mail.ru>
2023-03-13 19:44:04 +0700

    HW Done

---
+++ Task_10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+if ((a >= 100 && a < 1000) || (a <=-100 && a >= -1000))
+    {
+    int b = a / 10;
+    Console.WriteLine(Math.Abs(b % 10));
+    }
+else 
+    Console.WriteLine("Вы ввели не трехзначное число");
---
+++ Task_13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 100 && a > -100)
+    Console.WriteLine("Третьей цифры нет");
+if ((a < 1000 && a >= 100) || (a <= -100 && a > -1000))
+    Console.WriteLine(Math.Abs(a % 10));
+if ((a < 10000 && a >=1000) || (a <= -1000 && a > -10000))
+    Console.WriteLine(Math.Abs(a % 100/10));
+if ((a < 100000 && a >=10000) || (a <= -10000 && a > -100000))
+    Console.WriteLine(Math.Abs(a % 1000/100));
